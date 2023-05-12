74541101cba58609c9b31692ed76616e8559c05d
Oluwaseun <tanimolaoluwaseun70@gmail.com>
2023-05-12 22:40:47 +0100

    feat: user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,46 @@
+# Django Import
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-class User(AbstractUser):
-   id = models.UUIDField(primary_key=True,)
+class UserManager(BaseUserManager):
+
+   def create_user(self, email, password=None, **extra_fields):
+      if not email:
+         raise ValueError('The Email Field must be set')
+      email = self.normalize_email(email)
+      user = self.model(email=email, **extra_fields)
+      user.set_password(password)
+      user.save()
+      return user
+
+      def create_superuser(self, email, password=None, **extra_fields):
+         extra_fields.setdefault('is_staff', True)
+         extra_fields.setdefault('is_superuser', True)
+         return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser):
+   id = models.UUIDField(primary_key=True)
+   email = models.EmailField(unique=True)
+   password = models.CharField(max_length=120)
+   name = models.CharField(max_length=255)
+   created_at = models.DateTimeField(auto_now_add=True)
+   is_staff = models.BooleanField(default=False)
+   is_superuser = models.BooleanField(default=False)
+
+   objects = UserManager()
+
+   USERNAME_FIELD = 'email'
+   REQUIRED_FIELDS = ['name']
+
+   def __str__(self):
+      return self.email
+
+class Instructor(models.Model):
+   id = models.UUIDField(primary_key=True)
+   user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='instructor')
+   bio = models.CharField(max_length=250)
+   contact_info = models.CharField(max_length=250)
+   profile_picture = models.ImageField(upload_to='Unilearn\profile_pictures')
+
+   def __str__(self):
+      return self.user.email

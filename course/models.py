f2a70c5cc5d87e725f384782b31ecda4cfa51c02
Oluwaseun241 <tanimolaoluwaseun70@gmail.com>
2023-06-02 13:22:22 +0100

    feat: models

--- course/models.py
+++ course/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from shortuuid.django_fields import ShortUUIDField
+from accounts.model import Instructor 
 # Create your models here.
 
 class Course(models.Model):
     id = ShortUUIDField(primary_key=True)
-    instructor_id
-    course_code
+    instructor_id = models.ForeignKey(Instructor, on_delete=models.CASCADE, related_name='instructor_id')
+    course_code = models.CharField
     category
     price
     created_at

c5bc3b812e290a18f26addd7bbfeb77231f1aca5
Oluwaseun241 <tanimolaoluwaseun70@gmail.com>
2023-06-03 17:55:06 +0100

    chore: models course

--- course/models.py
+++ course/models.py
@@ -1,21 +1,21 @@
 from django.db import models
 from shortuuid.django_fields import ShortUUIDField
-from accounts.model import Instructor 
+from accounts.models import Instructor 
 # Create your models here.
 
 class Course(models.Model):
     id = ShortUUIDField(primary_key=True)
     instructor_id = models.ForeignKey(Instructor, on_delete=models.CASCADE, related_name='instructor_id')
-    course_code = models.CharField
-    category
-    price
-    created_at
-    updated_at
+    course_code = models.CharField(max_length=20)
+    category = models.CharField(max_length=30)
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
 class Material(models.Model):
     id = ShortUUIDField(primary_key=True)
-    title
-    description
-    course_id
-    file
-    created_at
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=150)
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE,  related_name='course_id')
+    file = models.FileField(upload_to=None)
+    created_at = models.DateTimeField(auto_now_add=True)
